1bd9638b0bd76fe260a8ac8c810f2ece35bef9ab
gazjoy <joyce.gareth@gmail.com>
2026-03-03 15:33:23 +0000

    Revert cursors auto fix, still works

--- _data/posts.js
+++ _data/posts.js
@@ -1,33 +1,9 @@
 const fetch = require("node-fetch");
-const fs = require("fs");
-
-const DEBUG_LOG_PATH = "/Users/gareth.joyce/Projects/phoenix/.cursor/debug.log";
 
 module.exports = async function () {
   console.log("Fetching data...");
 
   return fetch("https://cannockswimmingclub.org/wp-json/wp/v2/posts")
     .then((res) => res.json())
-    .then((json) => {
-      // #region agent log
-      const data = {
-        isArray: Array.isArray(json),
-        length: Array.isArray(json) ? json.length : 0,
-        sampleKeys: Array.isArray(json) && json[0] ? Object.keys(json[0]) : []
-      };
-      try {
-        fs.appendFileSync(
-          DEBUG_LOG_PATH,
-          JSON.stringify({
-            timestamp: Date.now(),
-            location: "_data/posts.js",
-            message: "posts fetch result",
-            hypothesisId: "H2",
-            data
-          }) + "\n"
-        );
-      } catch (e) {}
-      // #endregion
-      return json;
-    });
+    .then((json) => json);
 };
